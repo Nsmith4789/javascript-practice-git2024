7832696043bc99d19c94cd55602ab3d0a9c3428f
Nsmith4789 <156531219+Nsmith4789@users.noreply.github.com>
2024-03-10 22:31:33 -0500

    update week-7

---
+++ script.js
@@ -0,0 +1,41 @@
+geusseButton.onclick = function () {
+    //alert("Can You Geuss Who")
+    let isBald = baldCheckbox.checked
+    let hasTattoo = tattooCheckbox.checked
+    let hasEarring = earringCheckbox.checked
+    let hasGlasses = glassesCheckbox.checked
+    //alert(hasGlasses)
+    let isNick = hasGlasses && hasTattoo && !isBald && hasEarring;
+    let isTim = !hasGlasses && !hasTattoo && !isBald && hasEarring;
+    let isCaleb = hasGlasses && hasTattoo && !isBald && !hasEarring;
+    let isGary = hasGlasses && !hasTattoo && !isBald && hasEarring;
+    //alert(isNick)
+    if (isNick) {
+        alert("Im am Nick")
+        vacaImg.src = "flash.jpeg";
+        geussBody.style.backgroundColor = "blue"
+    } else if (isTim) {
+        alert("i am tim")
+        vacaImg.src = "allmight.jpeg";
+        geussBody.style.backgroundColor = "red"
+    } else if (isCaleb) {
+        alert("I am Caleb")
+        vacaImg.src = "greenlantern.jpeg";
+        geussBody.style.backgroundColor = "green"
+    } else if (isGary) {
+        alert("I Am Gary")
+        vacaImg.src = "superman.jpeg";
+        geussBody.style.backgroundColor = "yellow"
+    } else {
+        alert("unknown Person")
+    }
+    //let iam = document.querySelectorAll(".checkbox");
+    resetButton.onclick = function () {
+        let iam = ["isNick", "isGary", "isCaleb", "isTim"];
+        for (let i = 0; i > 4; i++) {
+            console.log(iam[i])
+
+        }
+
+    }
+}
